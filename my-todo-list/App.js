488eab26323c7ec51a1dc75500d500d305c57879
Bhavyat128 <bhavya.thalath@griffithuni.edu.au>
2024-03-22 21:44:04 +1000

    Milestone 1
    Milestone 1

--- my-todo-list/App.js
+++ my-todo-list/App.js
@@ -5,16 +5,16 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.title}>
-        <Text style={{ textAlign: 'center', fontSize: 30, fontWeight: '500' }}> My Todo List</Text>
+        <Text style={{ textAlign: 'center', fontSize: 35,fontStyle: 'italic', fontWeight: '500' }}> My Todo List</Text>
       </View>
-      <View style={{ flex:5, marginTop: 30 }}>
+      <View style={{ flex:7, marginTop: 10 }}>
         {list.map((s, i) => (
           <View style={styles.box} key={i}>
-            <Text style={{ color: 'black', marginLeft: 20, justifyContent: 'flex-start', alignSelf: 'center' }}>{s}</Text>
+            <Text style={{ color: 'black',fontSize:18, marginLeft: 20, justifyContent: 'flex-start', alignSelf: 'center' }}>{s}</Text>
           </View>
         ))}
       </View>
-      <View style={{ flex: 1, justifyContent: 'center', alignSelf: 'center', width: '100%', borderTopWidth: 2 }}>
+      <View style={{ flex: 1, height:'100%', justifyContent: 'center', alignSelf: 'center', width: '100%', borderTopWidth: 2 }}>
         <Text style={styles.chess}> Add New Todo </Text>
       </View>
     </View>
@@ -27,12 +27,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 45,
-    backgroundColor: '#fff',
+    backgroundColor: '#a5cf82',
     
   },
   title: {
     //fontSize: 30,
     flex: 1,
+    height: 20,
     justifyContent: 'center',
     alignContent: 'center',
     borderBottomWidth: 2,
@@ -45,17 +46,18 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignContent: 'center',
     margin: 5,
-    marginBottom: 50,
+    marginBottom: 40,
     fontWeight: '500',
     backgroundColor: '#4386E6',
     borderBottomWidth: 4,
     borderRadius: 5,
     textAlign: 'center',
+    height:'50%',
 
   },
   box: {
     height: 35,
-    backgroundColor: '#59E69C',
+    backgroundColor: '#5ab53c',
     margin: 5,
     flexDirection: 'row',
     borderRadius: 5,
